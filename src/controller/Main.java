51620265f8ae4bc8411785a62eae47b7c44f00b2
Hactually Benji <mrbenamiller@gmail.com>
2023-02-26 15:17:31 +0000

    Add final comments

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,11 +7,19 @@ import dto.Studio;
 import ui.DVDLibraryView;
 import ui.UserIO;
 
+/*
+Ben Miller - 2022-02-26
+ */
+
 public class Main {
     public static void main(String[] args) {
         DVDLibraryView view = new DVDLibraryView();
         DVDLibrary library = new DVDLibrary();
 
+        /*
+         * Create an App instance to begin the magic.
+         * view and library are injected as implementation class dependencies for App
+         */
         App app = new App(view, library);
         app.start();
     }
